3de8321bd96ac3a61a7cff098bc06be9e98c61f5
John Allan Hinds <jabistro@gmail.com>
2026-06-18 20:33:34 -0400

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,13 +35,11 @@ const skillsContent = document.getElementsByClassName('skills__content'),
       skillsHeader = document.querySelectorAll('.skills__header')
 
 function toggleSkills(){
-    let itemClass = this.parentNode.className
-    
-    for(i = 0; i < skillsContent.length; i++){
-        skillsContent[i].className = 'skills__content skills__close'
-    }
-    if(itemClass === 'skills__content skills__close'){
-        this.parentNode.className = 'skills__content skills__open'
+    const content = this.parentNode
+    if(content.className === 'skills__content skills__open'){
+        content.className = 'skills__content skills__close'
+    } else {
+        content.className = 'skills__content skills__open'
     }
 }
 
